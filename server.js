024c5ffe5baacdd3118ba512d11934e656aabbcb
Tomkc518 <32521131+Tomkc518@users.noreply.github.com>
2018-05-28 11:20:35 -0500

    updating to heroku connect

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mtgScraper");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mtgScraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 var exphbs = require("express-handlebars");
 
